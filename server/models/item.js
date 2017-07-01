0d3c7387c21302fbf8fa8aa1c32b87d73b786f0d
Keegan Gladstone <keegangladstone@gmail.com>
2017-07-01 09:06:39 -1000

    server, mongo, seeder

---
+++ server/models/item.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    imagePath: {type: String, required: true},
+    title: {type: String, required: true},
+    location: {type: String, required: true},
+    inventory: {type: Number, required: true},
+    price: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Item', itemSchema);
+
