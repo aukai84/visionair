32f6ebcb0b05ef667435cd0946620693cdac9bf3
Aukai Tirrell <mtirrell@hawaii.edu>
2017-07-11 21:00:02 -1000

    creating next app

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './containers/Home';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Home/>, document.getElementById('root'));
-registerServiceWorker();
