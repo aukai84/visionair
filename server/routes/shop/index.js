719bd68fb3c316620234ef1ce9d00a8c38632366
Keegan Gladstone <keegangladstone@gmail.com>
2017-07-25 19:03:39 -1000

    subtle changes

--- server/routes/shop/index.js
+++ server/routes/shop/index.js
@@ -5,24 +5,24 @@ const Item = require('../../models/item');
 
 //get complete list of items
 router.get('/', function(req, res, next) {
-    Item.find(function(err, items){
+    Item.find(function(err, items) {
         res.json(items);
     });
 });
 
-router.get('/:id', function(req, res, next){
+router.get('/:id', function(req, res, next) {
     let id = req.params.id;
-    Item.findById(id, function(err, item){
-        if (err){
+    Item.findById(id, function(err, item) {
+        if (err) {
             res.send(err);
         } else {
             res.send({
                 message: `This is the product page for item ${item._id}`,
                 item
-            })
+            });
         }
-    })
-})
+    });
+});
 
 
 
