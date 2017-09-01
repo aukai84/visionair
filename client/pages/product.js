7af0ec37ef8e2326ac30456602118bdc86c78ff4
Aukai Tirrell <mtirrell@hawaii.edu>
2017-08-31 23:06:10 -1000

    created product page with options

--- client/pages/product.js
+++ client/pages/product.js
@@ -4,17 +4,27 @@ import Layout from '../components/Layout.js';
 import fetch from 'isomorphic-unfetch';
 import {Container, Collapse, Row, Col} from 'reactstrap';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import AuthService from '../utils/AuthService.js';
+
+const auth = new AuthService('http://localhost:80800');
 
 class Product extends Component {
     constructor(props){
         super(props)
         this.state = {
             sizeDropDown: false,
-            materialDropDown: false
+            materialDropDown: false,
+            size: '',
+            material: '',
+            quantity: ''
         }
         this.toggleSize = this.toggleSize.bind(this);
         this.toggleMaterial = this.toggleMaterial.bind(this);
+        this.handleSize = this.handleSize.bind(this);
+        this.handleMaterial = this.handleMaterial.bind(this);
+        this.handleQuantity = this.handleQuantity.bind(this);
+        this.addToCart = this.addToCart.bind(this);
     }
 
     toggleSize(){
@@ -27,6 +37,29 @@ class Product extends Component {
         this.setState({
             materialDropDown: !this.state.materialDropDown
         })
+    }
+
+    handleSize(e){
+        console.log(e.target.value)
+        this.setState({
+            size: e.targer.value
+        })
+    }
+
+    handleMaterial(e){
+        this.setState({
+            material: e.target.value
+        })
+    }
+
+    handleQuantity(e){
+        this.setState({
+            quantity: e.target.value
+        })
+    }
+
+    addToCart(){
+        auth.fetch(`${auth.fetch}`)
     }
 
     render(){
@@ -45,30 +78,37 @@ class Product extends Component {
                             <img className="product-image" width="100%" src={this.props.item.imagePath}/>
                         </Col>
                         <Col xs="12" sm="12" md="4" lg="4">
-                            <div className="product-info">
+                            <div className="product-">
                                 <h6>$ {this.props.item.price}</h6> 
                                 <h2>{this.props.item.title}</h2>
                                 <p>{this.props.item.location}</p>
-                                <Dropdown width="100%" isOpen={this.state.sizeDropDown} toggle={this.toggleSize}>
-                                    <DropdownToggle caret>
-                                      SIZE 
-                                    </DropdownToggle>
-                                    <DropdownMenu className="dropdown-menu">
-                                        <DropdownItem>Small</DropdownItem>
-                                        <DropdownItem>Medium</DropdownItem>
-                                        <DropdownItem>Large</DropdownItem>
-                                    </DropdownMenu>
-                                </Dropdown>
-                                <Dropdown width="100%" isOpen={this.state.materialDropDown} toggle={this.toggleMaterial}>
-                                    <DropdownToggle caret>
-                                        MATERIAL
-                                    </DropdownToggle>
-                                    <DropdownMenu className="dropdown-meny">
-                                        <DropdownItem>Canvas</DropdownItem>
-                                        <DropdownItem>Fine Art Paper</DropdownItem>
-                                        <DropdownItem>Metal</DropdownItem>
-                                    </DropdownMenu>
-                                </Dropdown>
+                                <div id="select-size-container">
+                                    <p>SIZE</p>
+                                    <select onChange={this.handleSize}>
+                                        <option value="small">Small</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="large">Large</option>
+                                    </select>
+                                </div>
+                                <div id="select-material-container">
+                                    <p>MATERIAL</p>
+                                    <select onChange={this.handleMaterial}>
+                                        <option value="canvas">Canvas</option>
+                                        <option value="metalic">Metalic</option>
+                                        <option value="paper">Fine Art Paper</option>
+                                    </select>
+                                </div>
+                                <div id="select-quantity-container">
+                                    <p>QUANTITY</p>
+                                    <select onChange={this.handleQuantity}>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </div>
+                                <Button onClick={this.addtoCart}>ADD TO CART</Button>
                             </div>
                         </Col>
                     </Row>
